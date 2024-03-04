fa89cb6f74558642c4076a720257fd06b7b5bb35
David McKenzie-James <142780219+sagemmj@users.noreply.github.com>
2024-03-04 16:07:07 +0000

    feat: Added ClinicReviewLink component to display logos and feedback message.

---
+++ app/components/ClinicReviewLink.tsx
@@ -0,0 +1,106 @@
+import { Flex, Box, Image, VStack } from "@chakra-ui/react";
+import RefreshInfo from "~/components/RefreshInfo";
+
+const logos = [
+  {
+    id: "Google",
+    href: "https://www.realself.com/dr/paul-banwell-east-grinstead-united-kingdom#reviews",
+    src: "http://assets.vipflow.co.uk/files/realself-logo.jpg",
+    alt: "Google",
+  },
+  {
+    id: "Realself",
+    href: "https://www.google.com/maps/",
+    src: "http://assets.vipflow.co.uk/files/google-logo.png",
+    alt: "Google",
+  },
+];
+
+export default function ClinicReviewLink() {
+  return (
+    <VStack
+      bg="#fff"
+      mt="40px"
+      borderRadius="10px"
+      w={{ base: "100%", md: "700px" }}
+      textAlign={"center"}
+    >
+      <RefreshInfo />
+      <Flex direction="column" alignItems="center" width="100%" gap="10px">
+        <Flex
+          mb="5px"
+          fontSize={{ base: "28px", md: "28px" }}
+          fontWeight="600"
+          lineHeight={{ base: "1.2em", md: "1em" }}
+          textAlign="center"
+          w={{ base: "80%", md: "unset" }}
+        >
+          Great! We would love to hear your feedback.
+        </Flex>
+        <Flex
+          fontSize={{ base: "18px", md: "20px" }}
+          fontWeight="400"
+          textAlign="center"
+          lineHeight={{ base: "1.4em", md: "1.2em" }}
+          w={{ base: "90%", md: "420px" }}
+        >
+          We value all patient feedback and a review of any length would be
+          greatly appreciated!
+        </Flex>
+        <Flex
+          direction={{ base: "column", md: "row" }}
+          width="100%"
+          justifyContent={{ base: "unset", md: "center" }}
+          alignItems={{ base: "center", md: "unset" }}
+          gap={{ base: "25px", md: "20px" }}
+          py="20px"
+        >
+          {logos.map((logo) => (
+            <Box
+              as="a"
+              href={logo.href}
+              target="_blank"
+              id={logo.id}
+              key={logo.id}
+            >
+              <Image
+                src={logo.src}
+                alt={logo.alt}
+                height={{ base: "45px", md: "50px" }}
+                objectFit="contain"
+              />
+            </Box>
+          ))}
+        </Flex>
+        <Flex
+          mt="20px"
+          fontSize={{ base: "14px", md: "16px" }}
+          fontWeight="600"
+          color="#23385b"
+          textAlign="center"
+          justifyContent={"center"}
+          alignItems={"center"}
+          gap="3px"
+          lineHeight="1em"
+          w="90%"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18px"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+            />
+          </svg>
+          Click any logo to visit your preferred website.
+        </Flex>
+      </Flex>
+    </VStack>
+  );
+}
---
+++ app/routes/paulbanwell.tsx
@@ -0,0 +1,148 @@
+import { useState } from "react";
+import { Box, Flex, Image, VStack } from "@chakra-ui/react";
+import RestaurantReview from "~/formcomponents/RestaurantReview";
+import ClinicReviewLink from "~/components/ClinicReviewLink";
+
+// Company Branding & Details
+const brandBG = "#e5e5e5";
+const brandAccent = "#bfa70d";
+const brandLogo =
+  "https://www.paulebanwell.com/wp-content/themes/pb/img/logo.png";
+const companyImage =
+  "https://www.banwellclinic.com/wp-content/uploads/2015/12/pb-internal-banner.jpg";
+const companyName = "The Banwell Clinic";
+const companyStreet = "McIndoe Surgical Centre, Holtye Road";
+const companyCity = "East Grinstead,";
+const companyCountry = "United Kingdom";
+
+export default function PaulBanwell() {
+  const [displayComponent, setDisplayComponent] = useState("");
+  return (
+    <Flex
+      h="100vh"
+      w="100%"
+      justifyContent="start"
+      flexDirection="column"
+      overflowX="hidden"
+    >
+      <Box
+        bg={brandBG}
+        w="100%"
+        py="15px"
+        color="white"
+        borderTop="1px solid"
+        borderTopColor={brandAccent}
+      >
+        <Flex justifyContent="center">
+          <Image
+            height="40px"
+            objectFit="contain"
+            width="auto"
+            src={brandLogo}
+          />
+        </Flex>
+      </Box>
+      <VStack bg="#fff" h="100%">
+        {displayComponent === "" && (
+          <VStack
+            mt="40px"
+            id="choice"
+            bg="#fff"
+            borderRadius="10px"
+            w={{ base: "90%", md: "700px" }}
+            textAlign={"center"}
+          >
+            <Flex direction="column" alignItems="center" width="100%">
+              <Flex fontWeight="500" mb="25px" fontSize="24px" lineHeight="1em">
+                How was your experience?
+              </Flex>
+              <Flex
+                width="100%"
+                justifyContent={{ base: "space-between", md: "center" }}
+                alignItems="center"
+                gap={{ base: "0px", md: "30px" }}
+              >
+                <Flex
+                  id="negative"
+                  onClick={() => setDisplayComponent("Review")}
+                  flexDirection="column"
+                  alignItems="center"
+                  border="1px solid"
+                  bg="#fff"
+                  borderColor="#9a9a9a"
+                  borderRadius="10px"
+                  width={{ base: "48%", md: "200px" }}
+                  py="15px"
+                  gap="15px"
+                  cursor="pointer"
+                  _hover={{
+                    borderColor: "#23385b",
+                    bg: "#faf2e2",
+                  }}
+                >
+                  <Image
+                    src="http://assets.vipflow.co.uk/files/negative-review.png"
+                    alt="Negative"
+                    height={{ base: "60px", md: "80px" }}
+                  />
+                  <Flex
+                    fontWeight="500"
+                    fontSize="16px"
+                    lineHeight="1em"
+                    margin="0"
+                  >
+                    Could Be Better
+                  </Flex>
+                </Flex>
+                <Flex
+                  id="positive"
+                  onClick={() => setDisplayComponent("ReviewLink")}
+                  flexDirection="column"
+                  alignItems="center"
+                  border="1px solid"
+                  bg="#fff"
+                  borderColor="#9a9a9a"
+                  borderRadius="10px"
+                  width={{ base: "48%", md: "200px" }}
+                  py="15px"
+                  gap="15px"
+                  cursor="pointer"
+                  _hover={{
+                    borderColor: "#23385b",
+                    bg: "#effdea",
+                  }}
+                >
+                  <Image
+                    src="http://assets.vipflow.co.uk/files/positive-review.png"
+                    alt="Positive"
+                    height={{ base: "60px", md: "80px" }}
+                  />
+                  <Flex
+                    fontWeight="500"
+                    fontSize="16px"
+                    lineHeight="1em"
+                    margin="0"
+                  >
+                    Good Experience
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Flex>
+          </VStack>
+        )}
+        {displayComponent === "ReviewLink" && <ClinicReviewLink />}
+        {displayComponent === "Review" && (
+          <RestaurantReview
+            brandBG={brandBG}
+            brandAccent={brandAccent}
+            companyImage={companyImage}
+            companyName={companyName}
+            companyStreet={companyStreet}
+            companyCity={companyCity}
+            companyCountry={companyCountry}
+          />
+        )}
+      </VStack>
+    </Flex>
+  );
+}
