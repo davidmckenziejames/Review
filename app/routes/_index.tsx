ccbaec5563dbdb9067d6e722809c31229237cf30
David McKenzie-James <142780219+sagemmj@users.noreply.github.com>
2024-03-27 14:49:05 +0000

    new

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,13 @@
 import { Flex } from "@chakra-ui/react";
-import HomeHero from "~/home/HomeHero";
-import HomePreFeatures from "~/home/HomePreFeatures";
-import HomeFooter from "~/home/HomeFooter";
-import Marquee from "~/home/marquee";
-import Features from "~/home/Features";
-import Example from "~/home/Example";
 
 export default function ReviewHome() {
   return (
-    <Flex w="100%" h="100%" minH="100vh" flexDirection="column" bg="#fff">
-      <HomeHero />
-      <Marquee />
-      <Example />
-      <Features />
-      <HomePreFeatures />
-      <HomeFooter />
-    </Flex>
+    <Flex
+      w="100%"
+      h="100%"
+      minH="100vh"
+      flexDirection="column"
+      bg="#fff"
+    ></Flex>
   );
 }
